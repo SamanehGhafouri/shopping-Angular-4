84bb3c6d6a5f107a94505a6775f777d6e7f309d4
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-08-11 13:58:22 -0400

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  // firebase: {
+  //   apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
+  //   authDomain: "oshop-f3445.firebaseapp.com",
+  //   databaseURL: "https://oshop-f3445.firebaseio.com",
+  //   projectId: "oshop-f3445",
+  //   storageBucket: "oshop-f3445.appspot.com",
+  //   messagingSenderId: "223112932947"
+  // }
+
+  firebase: {
+    apiKey: "AIzaSyDh6tz7mhf49FfRzPES5pI2gkUfIKkHOfQ",
+    authDomain: "shopping-fe875.firebaseapp.com",
+    databaseURL: "https://shopping-fe875.firebaseio.com",
+    projectId: "shopping-fe875",
+    storageBucket: "shopping-fe875.appspot.com",
+    messagingSenderId: "297907653063",
+    appId: "1:297907653063:web:9b3a6ec622d3e7106331e2",
+    measurementId: "G-HB9T0CXB3J"
+  }
+
+};
