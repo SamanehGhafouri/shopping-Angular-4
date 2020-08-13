235dd5fa788d0eb7643e3933f647437931753569
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-08-13 15:12:08 -0400

    Displaying the Quantity and fixed images size

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ProductService} from "../product.service";
 import {ActivatedRoute} from "@angular/router";
 import {Product} from "../models/product";
 import 'rxjs/add/operator/switchMap';
+import {ShoppingCartService} from "../shopping-cart.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy{
   products: Product[] = [];
   category: string;
   filteredProducts: Product [];
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     rout: ActivatedRoute,
-    productService:ProductService) {
+    productService:ProductService,
+    private shoppingCartService: ShoppingCartService) {
+
 
     //first Observable
     productService
@@ -33,6 +39,15 @@ export class ProductsComponent {
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
+  }
+
+  async ngOnInit() {
+    (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 
