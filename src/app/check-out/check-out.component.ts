dd27f11160e7e4eddf61cfe3f31711cd57213e78
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-08-14 17:05:27 -0400

    Associated the order with the current user

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ShoppingCart} from "../models/shopping-cart";
+import {ShoppingCartService} from "../shopping-cart.service";
+import {Subscription} from "rxjs";
+import {OrderService} from "../order.service";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy{
+  shipping = {};
+  cart: ShoppingCart;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private shoppingCartService: ShoppingCartService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.authService.user$.subscribe(user => this.userId = user.uid);
   }
+
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+
+  placeOrder() {
+    let order = {
+      userId: this.userId,
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    };
+
+    this.orderService.storeOrder(order);
+  }
+
 
 }
