31dd7ade4236b7d7b78f5f83c269778a3fb42044
samaneh ghafouri <samanehghafouri@samanehs-MacBook-Pro.local>
2020-08-15 16:44:40 -0400

    Finished Check Out module

--- src/app/admin/admin-orders/admin-orders.component.ts
+++ src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderService} from "../../order.service";
 
 @Component({
   selector: 'app-admin-orders',
   templateUrl: './admin-orders.component.html',
   styleUrls: ['./admin-orders.component.css']
 })
-export class AdminOrdersComponent implements OnInit {
+export class AdminOrdersComponent{
+  orders$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private orderService: OrderService) {
+    this.orders$ = orderService.getOrders();
+    console.log("is Admin orders summary");
+    console.log(this.orders$);
   }
+
 
 }
